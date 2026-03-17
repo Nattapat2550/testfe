d5c15a431bfd5f2ecec702dd58975867241a4016
Nattapat2550 <nyansungvon@gmail.com>
2026-03-17 10:32:52 +0700

    1

--- frontend/src/libs/getUserProfile.tsx
+++ frontend/src/libs/getUserProfile.tsx
@@ -1,5 +1,9 @@
+// ไฟล์: src/libs/getUserProfile.tsx
+
+const BACKEND_URL = process.env.NEXT_PUBLIC_BACKEND_URL || "http://localhost:5000";
+
 export default async function getUserProfile(token: string) {
-    const response = await fetch("https://a08-venue-explorer-backend.vercel.app/api/v1/auth/me", {
+    const response = await fetch(`${BACKEND_URL}/api/v1/auth/me`, {
         method: "GET",
         headers: {
             authorization: `Bearer ${token}`,
